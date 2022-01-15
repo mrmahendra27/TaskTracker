eae0403eb09aaef5a73ea319c02f5fd6ba373593
Mahendra <mkhati47@gmail.com>
2022-01-15 13:44:48 +0530

    first commit

---
+++ src/components/AddTask.vue
@@ -0,0 +1,94 @@
+<template>
+    <form @submit="saveTask" class="add-form">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" name="text" v-model="text" placeholder="Add Task">    
+        </div>
+        <div class="form-control">
+            <label>Add Date and Time</label>
+            <input type="text" name="day" v-model="day" placeholder="Add Day and Time">    
+        </div>
+        <div class="form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" name="reminder" v-model="reminder">    
+        </div>
+        <input type="submit" value="Save Task" class="btn btn-block">
+    </form>    
+</template>
+
+<script>
+export default {
+    name: 'AddTask',
+    data() {
+        return {
+            text: '',
+            day: '',
+            reminder: false
+        }
+    },
+    methods: {
+        saveTask(e){
+            e.preventDefault()
+
+            if(!this.text){
+                alert('Please add a task!!!')
+                return
+            }
+
+            if(!this.day){
+                alert('Please add day!!!')
+                return
+            }
+
+            const addTask = {
+                // id: Math.floor(Math.random() * 10000),
+                text: this.text,
+                day: this.day,
+                reminder: this.reminder,    
+            }
+
+            this.$emit('save-task', addTask)
+            this.text = ''
+            this.day = ''
+            this.reminder = false
+        }
+    },
+}
+</script>
+
+<style scoped>
+.add-form{
+    margin-bottom: 50px;
+}
+.form-control {
+    margin: 20px 0px;
+}
+
+.form-control label {
+    display: block;
+}
+
+.form-control input {
+    height: 40px;
+    width: 100%;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 15px;
+}
+
+.form-control-check {
+    margin: 20px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.form-control-check label {
+    flex: 1;
+}
+
+.form-control-check input {
+    flex: 2;
+    height: 15px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+  emits: ["toggle-add"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
